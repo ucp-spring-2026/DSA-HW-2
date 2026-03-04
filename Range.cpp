7bfdf4ddd4ac97891f9b460c8186d0ac35bb1afc
Fareeha Iqbal <fareeha.iqbal@ucp.edu.pk>
2026-03-04 16:16:16 +0500

    Q4 uploaded

---
+++ Range.cpp
@@ -0,0 +1,12 @@
+/****  Q4. Design a template class Range<T> in C++ that represents a numeric range and checks whether a given value falls within that range.
+Your class should:
+•	Store two values of generic type T representing the minimum and maximum of the range, passed through the constructor.
+•	Provide the following member function: 
+o	contains(T value) → Returns true if the given value lies within the range (inclusive), and false otherwise.
+In your main() function, demonstrate the class by:
+•	Creating a Range<int> object and checking whether several integer values fall inside the range.
+•	Creating a Range<double> object and checking whether several decimal values fall inside the range.
+•	Creating a Range<char> object and checking whether certain characters fall within a character range (e.g., 'A' to 'Z').
+**/
+
+
