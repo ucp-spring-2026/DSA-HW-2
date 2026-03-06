425cdaf6c371b35206d10757313b6a12ae6cc915
ShazilMalik <l1f24bscs0932@ucp.edu.pk>
2026-03-06 21:58:44 +0500

    Update Array.cpp

--- Array.cpp
+++ Array.cpp
@@ -1,36 +1,66 @@
-/***  Q2. Write a template class Array<T> in C++ that can store exactly 5 elements of any data type.
-Your class should provide the following functionality:
-•	set(int index, T value) → Stores a value at the given index. If the index is out of range, display an appropriate message.
-•	get(int index) → Returns the value at the given index. If the index is out of range, display an appropriate message.
-•	print() → Displays all 5 elements in a single line.
-In your main() function, demonstrate the class by:
-•	Creating an Array<int> object, storing 5 integer values and printing them.
-•	Creating an Array<double> object, storing 5 decimal values and printing them.
-**/
 
-include<iostream>
+#include<iostream>
 using namespace std;
 
 template <typename T>
-class Array{
-	
+class Array {
+
 	T arr[5];
-	
-	public:
+
+public:
 	void set(int index, T value);
 	T get(int index);
-	void print ();	
+    void print();
 };
+template <typename T>
+void  Array<T> ::set(int index, T value)
+{
+	if (index >= 5)
+	{
+		cout << "Index is out of array size.the value cannot be allocated to the index." << endl;
+		return;
+	}
 
+		arr[index] = value;
+		index++;
+
+}
 //write function definitions of the class here
+template <typename T>
+T Array<T> ::get(int index)
+{
+	if (index >= 5)
+	{
+		cout << "Index is out of array size.there is no value at this index." << endl;
+		return -1;
+	}
 
-
-
+	return arr[index];
+}
+template <typename T>
+void Array<T> ::print()
+{
+	for (int i = 0; i < 5; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
 
 //Complete the main function code
 int main()
 {
+	Array<int> A;
+	A.set(3, 45);
+	A.set(2, 49);	
+	A.set(0, 41);
+	A.set(1, -23);
+	A.set(4, 9912);
+	cout << "Value at the index no.4:" << A.get(4) << endl;
+	cout << "Value at the index no.2:" << A.get(2) << endl;
 	
-	
+	A.print();
+
+
 	return 0;
 }
